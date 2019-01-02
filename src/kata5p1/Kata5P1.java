4fe284001d42730786ba067b48bb6735003bd935
Janet <Janet@Janet-PC>
2019-01-02 01:08:24 +0000

    Kata5P1 V3

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -3,22 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package kata5p1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Kata5P1 {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
         Class.forName("org.sqlite.JDBC");
         Connection con = DriverManager.getConnection("jdbc:sqlite:Kata5.db");
-
-        Statement st = con.createStatement();
-        ResultSet executeQuery =st.executeQuery("create table a ('Id' integer primary key autoincrement, 'Mail' text not null);");
         
-    }       
+        Statement st = con.createStatement();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("email.txt")));
+        System.out.println(st.executeQuery("select count(*) from a").getInt(1));
+    }
 }
+    
